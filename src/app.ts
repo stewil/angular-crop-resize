4288280c9f99acc053fc9ab31da000116f6592c1
Steve Wilcox <steve.wilcox@outlook.com.au>
2016-02-14 18:43:09 +1100

    Added core application module.

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+module ngCropResize.App {
+    "use strict";
+
+    export class AppBuilder {
+
+        public static instance : AppBuilder;
+        public angularModule   : ng.IModule;
+
+        constructor(name:string) {
+            AppBuilder.instance = this;
+
+            this.angularModule = angular.module(name, []);
+        }
+        public start(){
+            angular.element(document).ready(()=>{
+                angular.bootstrap(document, [this.angularModule.name]);
+            });
+        }
+    }
+
+    new ngCropResize.App.AppBuilder('ngCropResize').start();
+}
